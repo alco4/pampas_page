142e4bd0671c962290777660b0a4ad162a5047e1
Alan Corcos <acorcos@tecnosoftware.com>
2019-08-13 10:58:14 -0300

    first commit

---
+++ build/precache-manifest.7f7a4f67168f358ca3faa9e806a621ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d130fb057d35ddc9d889fbf1d68904dd",
+    "url": "/pampas_page/index.html"
+  },
+  {
+    "revision": "9a922a0e2f539e65a5ab",
+    "url": "/pampas_page/static/css/2.4ed46a55.chunk.css"
+  },
+  {
+    "revision": "d0a5329e50df718bea2e",
+    "url": "/pampas_page/static/css/main.4fce828f.chunk.css"
+  },
+  {
+    "revision": "9a922a0e2f539e65a5ab",
+    "url": "/pampas_page/static/js/2.7aecf123.chunk.js"
+  },
+  {
+    "revision": "d0a5329e50df718bea2e",
+    "url": "/pampas_page/static/js/main.3d130bb1.chunk.js"
+  },
+  {
+    "revision": "49c8575b9378fd6a5ed9",
+    "url": "/pampas_page/static/js/runtime~main.66197876.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pampas_page/static/media/logo.5d5d9eef.svg"
+  }
+]);
